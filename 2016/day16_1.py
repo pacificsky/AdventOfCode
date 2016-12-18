7f6c25ecda7f5592e4b2748ed93c22995cfa9c5c
Aakash Kambuj <ak13@kambuj.net>
2016-12-17 23:48:28 -0800

    Solutions for day 16

---
+++ 2016/day16_1.py
@@ -0,0 +1,55 @@
+# http://adventofcode.com/2016/day/16
+
+DISK_SIZE = 272
+INITIAL_INPUT = "01111001100111011"
+
+#DISK_SIZE = 20
+#INITIAL_INPUT = "10000"
+
+
+
+def main():
+    data = [int(x) for x in INITIAL_INPUT]
+
+    while len(data) < DISK_SIZE:
+        # inverse while copying
+        clone = [1 - bit for bit in data]
+        clone.reverse()
+        data = data + [0] + clone
+
+    data = data[:DISK_SIZE]
+    assert len(data) == DISK_SIZE
+
+    # Initialize checksum with the same info as data
+    checksum = [x for x in data]
+
+    while True:
+        index = 0
+        final_checksum = []
+        while index < (len(checksum) - 1):
+            if checksum[index] == checksum[index + 1]:
+                final_checksum.append(1)
+            else:
+                final_checksum.append(0)
+
+            index += 2
+
+        checksum = final_checksum
+        # Repeat if length is even
+        if len(checksum) % 2 == 1:
+            break
+
+    # checksum now has the final value
+    str_checksum = ""
+    for x in checksum:
+        str_checksum += str(x)
+
+    print str_checksum
+
+
+
+
+
+
+
+main()
