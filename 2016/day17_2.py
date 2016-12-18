3a927164c4c46902c385c9c0b62c14abd61c4057
Aakash Kambuj <ak13@kambuj.net>
2016-12-17 23:48:43 -0800

    Solutions for day 17

---
+++ 2016/day17_2.py
@@ -0,0 +1,89 @@
+# http://adventofcode.com/2016/day/17
+import Queue
+import hashlib
+
+GRID_WIDTH = 4
+GRID_HEIGHT = 4
+PASSCODE = "mmsxrhfx"
+TARGET = (3,3)
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
+DIRECTIONS_STR = ['U', 'D', 'L', 'R']
+
+class Room:
+    def __init__(self, x, y, path):
+        self.x = x
+        self.y = y
+        self.path = path
+
+
+def run(queue):
+
+    solutions = []
+    while queue.qsize() > 0:
+        room = queue.get()
+
+        print "Evaluating room ({0}, {1}) with path {2}".format(room.x, room.y, room.path)
+
+        if (room.x, room.y) == TARGET:
+            solutions.append(room)
+            continue
+
+        passcode = PASSCODE
+        for x in room.path:
+            passcode += x
+
+        print "Passcode is {0}".format(passcode)
+
+        hasher = hashlib.md5()
+        hasher.update(passcode)
+        hash = hasher.hexdigest().lower()
+
+        door_state = hash[:4]
+        print "Door state is {0}".format(door_state)
+
+        for direction in DIRECTIONS:
+            x = room.x + direction[0]
+            y = room.y + direction[1]
+
+            if x < 0 or x >= GRID_WIDTH or y < 0 or y >= GRID_HEIGHT:
+                print "Door ({0}, {1}) is outside bounds".format(x, y)
+                continue
+
+            index = DIRECTIONS.index(direction)
+            door_open = door_state[index] in ['b', 'c', 'd', 'e', 'f']
+
+            if door_open:
+                queue.put(Room(x, y, room.path + [DIRECTIONS_STR[index]]))
+                print "Door ({0}, {1}) is open because of door state {2}".format(x, y, door_state[index])
+            else:
+                print "Door ({0}, {1}) is closed because of door state {2}".format(x, y, door_state[index])
+
+    return solutions
+
+def get_path_string(path):
+    str = ""
+    for x in path:
+        str += x
+    return str
+
+def main():
+
+    queue = Queue.Queue()
+    queue.put(Room(0, 0, []))
+
+    solutions = run(queue)
+
+    if len(solutions) > 0:
+        for solution in solutions:
+            print "Path of length {0}".format(len(solution.path))
+
+    else:
+        print "No path possible"
+
+
+main()
